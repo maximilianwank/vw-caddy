673b14a7fdbb6a54612b86f8e9c3f0ba40323f93
Maximilian Wank <maxi.wank@gmx.de>
2024-05-14 15:27:40 +0200

    create_files.sh

---
+++ create_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download and move Caddyfile
+curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/Caddyfile
+sudo mkdir /srv/caddy
+sudo mv Caddyfile /srv/caddy/Caddyfile
+
+# Download the files
+curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/docker-compose.yaml
+mkdir ~/vw_caddy
+mv docker-compose.yaml ~/vw_caddy/docker-compose.yaml
