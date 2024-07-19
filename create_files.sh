7bf51eeb1bff89213b120f3e37d601f88ab23863
Maximilian Wank <maxi.wank@gmx.de>
2024-07-19 12:25:39 +0200

    automatically move fail2ban config files on deploy

--- create_files.sh
+++ create_files.sh
@@ -14,4 +14,10 @@ chown -R ec2-user:ec2-user /home/ec2-user/vw_caddy
 
 # fail2ban filter
 curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/filter_vaultwarden.local
+mv filter_vaultwarden.local /etc/fail2ban/filter.d/vaultwarden.local
+chown ec2-user:ec2-user /etc/fail2ban/filter.d/vaultwarden.local
+
+# fail2ban jail
 curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/jail_vaultwarden.local
+mv jail_vaultwarden.local /etc/fail2ban/jail.d/vaultwarden.local
+chown ec2-user:ec2-user /etc/fail2ban/jail.d/vaultwarden.local
