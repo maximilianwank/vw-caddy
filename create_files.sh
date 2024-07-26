1a3f3fc52a601ee1577134210da4f898dd84ba37
Maximilian Wank <maxi.wank@gmx.de>
2024-07-26 15:41:58 +0200

    fix generation of fail2ban files

--- create_files.sh
+++ create_files.sh
@@ -14,20 +14,20 @@ chown -R ec2-user:ec2-user /home/ec2-user/vw_caddy
 
 # fail2ban filter for user login
 curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/filter_vaultwarden-user.local
-mv filter_vaultwarden.local /etc/fail2ban/filter.d/vaultwarden-user.local
+mv filter_vaultwarden-user.local /etc/fail2ban/filter.d/vaultwarden-user.local
 chown ec2-user:ec2-user /etc/fail2ban/filter.d/vaultwarden-user.local
 
 # fail2ban jail for user login
 curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/jail_vaultwarden-user.local
-mv jail_vaultwarden.local /etc/fail2ban/jail.d/vaultwarden-user.local
+mv jail_vaultwarden-user.local /etc/fail2ban/jail.d/vaultwarden-user.local
 chown ec2-user:ec2-user /etc/fail2ban/jail.d/vaultwarden-user.local
 
 # fail2ban filter for admin login
 curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/filter_vaultwarden-admin.local
-mv filter_vaultwarden.local /etc/fail2ban/filter.d/vaultwarden-admin.local
+mv filter_vaultwarden-admin.local /etc/fail2ban/filter.d/vaultwarden-admin.local
 chown ec2-user:ec2-user /etc/fail2ban/filter.d/vaultwarden-admin.local
 
 # fail2ban jail for admin login
 curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/jail_vaultwarden-admin.local
-mv jail_vaultwarden.local /etc/fail2ban/jail.d/vaultwarden-admin.local
+mv jail_vaultwarden-admin.local /etc/fail2ban/jail.d/vaultwarden-admin.local
 chown ec2-user:ec2-user /etc/fail2ban/jail.d/vaultwarden-admin.local
