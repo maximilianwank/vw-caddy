e584ad7f97a1dccd7b27762e1153c2ecf2da9664
Maximilian Wank <maxi.wank@gmx.de>
2024-07-26 15:05:01 +0200

    fail2ban user + admin

--- create_files.sh
+++ create_files.sh
@@ -12,15 +12,22 @@ mkdir /home/ec2-user/vw_caddy
 mv docker-compose.yaml /home/ec2-user/vw_caddy/docker-compose.yaml
 chown -R ec2-user:ec2-user /home/ec2-user/vw_caddy
 
-# fail2ban filter
-curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/filter_vaultwarden.local
-mv filter_vaultwarden.local /etc/fail2ban/filter.d/vaultwarden.local
-chown ec2-user:ec2-user /etc/fail2ban/filter.d/vaultwarden.local
+# fail2ban filter for user login
+curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/filter_vaultwarden-user.local
+mv filter_vaultwarden.local /etc/fail2ban/filter.d/vaultwarden-user.local
+chown ec2-user:ec2-user /etc/fail2ban/filter.d/vaultwarden-user.local
 
-# fail2ban jail
-curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/jail_vaultwarden.local
-mv jail_vaultwarden.local /etc/fail2ban/jail.d/vaultwarden.local
-chown ec2-user:ec2-user /etc/fail2ban/jail.d/vaultwarden.local
+# fail2ban jail for user login
+curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/jail_vaultwarden-user.local
+mv jail_vaultwarden.local /etc/fail2ban/jail.d/vaultwarden-user.local
+chown ec2-user:ec2-user /etc/fail2ban/jail.d/vaultwarden-user.local
 
-# restart fail2ban
-sudo systemctl restart fail2ban
+# fail2ban filter for admin login
+curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/filter_vaultwarden-admin.local
+mv filter_vaultwarden.local /etc/fail2ban/filter.d/vaultwarden-admin.local
+chown ec2-user:ec2-user /etc/fail2ban/filter.d/vaultwarden-admin.local
+
+# fail2ban jail for admin login
+curl -O https://raw.githubusercontent.com/maximilianwank/vw-caddy/deploy/configs/jail_vaultwarden-admin.local
+mv jail_vaultwarden.local /etc/fail2ban/jail.d/vaultwarden-admin.local
+chown ec2-user:ec2-user /etc/fail2ban/jail.d/vaultwarden-admin.local
